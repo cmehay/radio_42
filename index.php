987e46b79c76d3fbfe09f62be364799dd7511458
Christophe Mehay <cmehay@student.42.fr>
2014-02-18 01:15:58 +0100

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Radio quarante-deux</title>
+	<link rel="stylesheet" href="style.css">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<script src="js/jquery-2.10.min.js"></script>
+	<script src="js/radio.js"></script>
+</head>
+<body>
+	<h1>Radio quarante-deux</h1>
+	<audio preload volume="0.0" src="http://42.ham-radio-op.net:8080"></audio>
+	<div id="player_button">
+		<div id="play_button"><i class="fa fa-play"></i></div>
+		<div id="pause_button" class="hidden"><i class="fa fa-pause"></i></div>
+	</div>
+</body>
+</html>
---
+++ js/radio.js
@@ -0,0 +1,42 @@
+(function()
+{
+	"use strict";
+	var radio = {};
+	var priv = {};
+
+	priv.play = function()
+	{
+		$("#pause_button").addClass('hidden');
+		$("#play_button").removeClass('hidden');
+		$("audio").play();
+		$("audio").animate({volume: 1.0}, 1000);
+	};
+
+	priv.pause = function()
+	{
+		$("#pause_button").removeClass('hidden');
+		$("#play_button").addClass('hidden');
+		$("audio").animate({volume: 0.0}, 1000);
+		setTimeout(function()
+		{
+			$("audio").pause();
+		}, 1000);
+	}
+	radio.onready = function(){
+		$("#play_button").click(function()
+		{
+			priv.play();
+		});
+
+		$("#pause_button").click(function()
+		{
+			priv.pause();
+		});
+	};
+
+	window.radio = radio;
+})();
+
+$(document).ready(function(){
+	radio.onready();
+});
