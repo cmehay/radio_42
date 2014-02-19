5062042a07a685f3d213d493ed3b32e3dbafefd3
Christophe Mehay <cmehay@student.42.fr>
2014-02-19 01:08:48 +0100

    font

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Radio quarante-deux</title>
 	<link rel="stylesheet" href="style/style.css">
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
@@ -9,11 +10,12 @@
 	<script src="js/radio.js"></script>
 </head>
 <body>
-	<h1>Radio quarante-deux</h1>
+	<h4>$> ./radio_42</h4>
 	<audio preload volume="0.0" src="http://42.ham-radio-op.net:8080"></audio>
 	<div id="player_button">
 		<div id="play_button"><i class="fa fa-play"></i></div>
 		<div id="pause_button" class="hidden"><i class="fa fa-pause"></i></div>
 	</div>
+	<div id="round"></div>
 </body>
 </html>
