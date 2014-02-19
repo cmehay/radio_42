423f2038b22d8a4a9e163d3dbf4f390bceadabd9
Christophe Mehay <cmehay@student.42.fr>
2014-02-19 19:14:46 +0100

    center

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Radio quarante-deux</title>
 	<link rel="stylesheet" href="style/style.css">
-	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 	<script src="js/jquery-2.1.0.min.js"></script>
 	<script src="js/radio.js"></script>
 </head>
@@ -14,8 +13,8 @@
 	<audio preload volume="0.0" src="http://42.ham-radio-op.net:8080"></audio>
 	<div id="englobe">
 		<div id="player_button">
-			<div id="play_button"><i class="fa fa-play"></i></div>
-			<div id="pause_button" class="hidden"><i class="fa fa-pause"></i></div>
+			<div id="play_button"></div>
+			<div id="pause_button" class="hidden"></div>
 			<div id="round"></div>
 		</div>
 	</div>
