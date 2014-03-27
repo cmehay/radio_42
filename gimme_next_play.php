aa5dbf454627acfa21bf5dbc1e5a1350a5cfa42b
Christophe Mehay <cmehay@student.42.fr>
2014-03-27 16:19:20 +0100

    test next_play

--- gimme_next_play.php
+++ gimme_next_play.php
@@ -2,7 +2,7 @@
 
 $list_jobs = shell_exec('sudo /opt/sbin/list_radio_jobs');
 
-$list_jobs = str_replace(" ", "\t", $list_jobs);
+$list_jobs = str_replace("\t", " ", $list_jobs);
 
 $list_jobs = explode("\n", $list_jobs);
 
