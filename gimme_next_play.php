f0049131a923350e6ac4eaf709ae76a39d385a55
Christophe Mehay <cmehay@student.42.fr>
2014-03-27 16:10:18 +0100

    test next_play

---
+++ gimme_next_play.php
@@ -0,0 +1,21 @@
+<?php
+
+$list_jobs = shell_exec('sudo /opt/sbin/list_radio_jobs');
+
+$list_jobs = explode("\n", $list_jobs);
+
+foreach ($list_jobs as $idx => $jobs)
+{
+	$list_jobs[$idx] = explode(' ', $jobs);
+}
+
+foreach ($list_jobs as $key => $value)
+{
+	foreach ($value as $key2 => $value2) {
+		echo "$value2\n";
+	}
+	echo "\n";
+}
+
+
+?>
