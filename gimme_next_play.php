72329ac4af052cc062834ce672da9dcbff6144cc
Christophe Mehay <cmehay@student.42.fr>
2014-03-27 16:17:14 +0100

    test next_play

--- gimme_next_play.php
+++ gimme_next_play.php
@@ -2,7 +2,7 @@
 
 $list_jobs = shell_exec('sudo /opt/sbin/list_radio_jobs');
 
-str_replace($list_jobs, "\t", ' ');
+str_replace(' ', "\t", $list_jobs);
 
 $list_jobs = explode("\n", $list_jobs);
 
@@ -18,6 +18,5 @@ foreach ($list_jobs as $key => $value)
 	}
 	echo "\n";
 }
-
 
 ?>
