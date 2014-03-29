16d06ed9390540c247895400a0202cc73f213e0a
Christophe Mehay <cmehay@student.42.fr>
2014-03-29 18:43:03 +0100

    wip

--- gimme_next_play.php
+++ gimme_next_play.php
@@ -4,7 +4,7 @@ define(RADIO_USER, 'radio');
 define(LIST_RADIO_JOBS, 'sudo /opt/sbin/list_radio_jobs');
 define(GET_RADIO_JOBS, 'sudo /opt/sbin/get_radio_jobs');
 define(GET_EMISSION_INFO, 'sudo /opt/sbin/get_emission_info');
-define(PLAY_EMISSION, 'play_emission');
+define(PLAY_EMISSION, 'play_emission ');
 
 $list_jobs = shell_exec(LIST_RADIO_JOBS);
 
@@ -23,14 +23,23 @@ foreach ($list_jobs as $value)
 	if ($value[7] == RADIO_USER)
 	{
 		$job_idx[$i]['idx'] = $value[0];
-		$job_idx[$i++]['date'] = $value[1] . ' ' . $value[2] . ' ' .
-			$value[3] . ' ' . $value[4] . ' ' . $value[5];
+		$job_idx[$i++]['timestamp'] =
+			strtotime($value[3] . '/' . $value[2] . '/' . $value[5] .
+				':' . $value[4]);
 	}
+}
+
+if (!isset($job_idx))
+{
+	$json_return['jobs_num'] = 0;
+	echo(json_encode($json_return));
+	exit ;
 }
 
 foreach ($job_idx as $key => $value)
 {
-	$job_idx[$key]['cmd'] = shell_exec(GET_RADIO_JOBS . ' ' . $value);
+	$job_idx[$key]['cmd'] = shell_exec(GET_RADIO_JOBS .
+		' ' . $job_idx[$key]['idx']);
 	$job_idx[$key]['cmd'] = explode("\n", $job_idx[$key]['cmd']);
 	foreach ($job_idx[$key]['cmd'] as $content)
 	{
@@ -48,12 +57,15 @@ foreach ($job_idx as $key => $value)
 		foreach ($job_idx[$key]['file_info'] as $info)
 		{
 			if (strpos($info, 'TPE1'))
-				$job_idx[$key]['artist'] = strstr($info, ':');
+				$json_return[$key]['artist'] = strstr($info, ':');
 			elseif (strpos($info, 'TIT2'))
-				$job_idx[$key]['title'] = strstr($info, ':');
+				$json_return[$key]['title'] = strstr($info, ':');
 		}
+		$json_return['jobs_num'] = $key + 1;
+		$json_return[$key]['timestamp'] = $job_idx[$key]['timestamp'];
 	}
-	
 }
+
+echo(json_encode(json_return));
 
 ?>
