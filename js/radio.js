5b0318fc34947bbb2fe8da6701c18a4aea495a06
cmehay <cmehay@student.42.fr>
2014-03-31 00:22:23 +0200

    test display jobs

--- js/radio.js
+++ js/radio.js
@@ -138,7 +138,7 @@
 		var minutes = date.getMinutes();
 		var ampm = (hours >= 12) ? 'pm' : 'am';
 		hours = hours % 12;
-		hours = (hours ? hours) : 12;
+		hours = (hours) ? hours : 12;
 		minutes = (minutes < 10) ? '0'+minutes : minutes;
 		var strTime = hours + ':' + minutes + ' ' + ampm;
 		return (strTime);
